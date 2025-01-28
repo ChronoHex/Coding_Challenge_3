779acbba83200263fcd1a565b971df6d388302e4
Eugene Rodriguez <er11@usf.edu>
2025-01-28 13:14:07 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,11 @@ prices.push(70); // Add new price to the end of the array
 prices.shift(); // Remove the first price from the array
 
 console.log("Updated prices: ", prices); // Print the updated array of prices
+
+// task 2 - Modifting Customer Orders //
+let orders = [100, 200, 300, 400, 500]; // orders array
+orders[2] += 5; // Increase the 3rd order by 5
+let totalOrders = orders.reduce((total, orders) => total + orders, 0); // Calculate the total of all orders
+
+console.log("Updated orders: ", orders); // Print the updated array of orders
+console.log("Total order count: ", totalOrders); // Print the total of all orders
