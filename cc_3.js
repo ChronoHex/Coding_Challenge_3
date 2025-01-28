df5dc2541fa80f6bbb03e0afdc5b05b24616b1f9
Eugene Rodriguez <er11@usf.edu>
2025-01-28 13:01:58 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// task 1 - Product Price Management //
+let prices = [20, 30, 40, 50, 60]; // prices array
+prices.push(70); // Add new price to the end of the array
+prices.shift(); // Remove the first price from the array
+
+console.log("Updated prices: ", prices); // Print the updated array of prices
